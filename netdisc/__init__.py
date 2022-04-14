a3a48ae62a2303dbce99a9395a0823f02b16d6cd
Codie Jones <codiejones1@gmail.com>
2022-04-14 14:09:50 -0700

    expose merge_networks

--- netdisc/__init__.py
+++ netdisc/__init__.py
@@ -5,3 +5,4 @@ version = pkg_resources.require("netdisc")[0].version
 from .juniper import get_juniper_networks
 from .cisco_nxos import get_nxos_networks
 from .paloalto_panos import get_panos_networks
+from .common import merge_networks
