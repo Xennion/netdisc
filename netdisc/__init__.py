f1e59bf06264f0ab5d5de7f1571af0960403eb3f
Codie Jones <codiejones1@gmail.com>
2022-04-14 14:11:26 -0700

    expore network schema

--- netdisc/__init__.py
+++ netdisc/__init__.py
@@ -6,3 +6,4 @@ from .juniper import get_juniper_networks
 from .cisco_nxos import get_nxos_networks
 from .paloalto_panos import get_panos_networks
 from .common import merge_networks
+from .network import NetworkSchema
